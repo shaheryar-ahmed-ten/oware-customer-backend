41eb11919b8d7b47c7372b36537c35edbfed1de9
Yousha Rizvi <yousharizvi@gmail.com>
2021-05-26 16:19:25 +0500

    Add seeder for roles and permission access

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const authService = require('./services/auth.service');
-const mailer = require('./services/mailer.service')
+const { isLoggedIn, checkPermission } = require('./services/auth.service');
+const { syncPermissions } = require('./services/permission.service');
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
-const publicRouter = require('./routes/public');
-const customerRouter = require('./routes/customer');
 const categoryRouter = require('./routes/category');
 const uomRouter = require('./routes/uom');
 const brandRouter = require('./routes/brand');
@@ -20,9 +18,6 @@ const productInwardRouter = require('./routes/productInward');
 const dispatchOrderRouter = require('./routes/dispatchOrder');
 const productOutwardRouter = require('./routes/productOutward');
 const inventoryRouter = require('./routes/inventory');
-const customerInquiryRouter = require('./routes/customerInquiry');
-
-
 
 const app = express();
 
@@ -39,18 +34,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/', indexRouter);
 app.use('/api/v1/user', userRouter);
-app.use('/api/v1/public', publicRouter);
-app.use('/api/v1/customer', authService.isLoggedIn, customerRouter);
-app.use('/api/v1/category', authService.isLoggedIn, categoryRouter);
-app.use('/api/v1/customer-inquiry', authService.isLoggedIn, customerInquiryRouter);
-app.use('/api/v1/uom', authService.isLoggedIn, uomRouter);
-app.use('/api/v1/brand', authService.isLoggedIn, brandRouter);
-app.use('/api/v1/warehouse', authService.isLoggedIn, warehouseRouter);
-app.use('/api/v1/product', authService.isLoggedIn, productRouter);
-app.use('/api/v1/product-inward', authService.isLoggedIn, productInwardRouter);
-app.use('/api/v1/dispatch-order', authService.isLoggedIn, dispatchOrderRouter);
-app.use('/api/v1/product-outward', authService.isLoggedIn, productOutwardRouter);
-app.use('/api/v1/inventory', authService.isLoggedIn, inventoryRouter);
+app.use('/api/v1/category', isLoggedIn, categoryRouter);
+app.use('/api/v1/uom', isLoggedIn, uomRouter);
+app.use('/api/v1/brand', isLoggedIn, brandRouter);
+app.use('/api/v1/warehouse', isLoggedIn, warehouseRouter);
+app.use('/api/v1/product', isLoggedIn, productRouter);
+app.use('/api/v1/product-inward', isLoggedIn, productInwardRouter);
+app.use('/api/v1/dispatch-order', isLoggedIn, dispatchOrderRouter);
+app.use('/api/v1/product-outward', isLoggedIn, productOutwardRouter);
+app.use('/api/v1/inventory', isLoggedIn, inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -70,6 +62,8 @@ app.use('/api/v1', function (err, req, res, next) {
     message: 'error'
   });
 });
+
+syncPermissions();
 
 // error handler
 app.use(function (err, req, res, next) {
---
+++ enums/roles.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+  CUSTOMER_SUPER_ADMIN: 'Customer Super Admin',
+  CUSTOMER_ADMIN: 'Customer Admin'
+});
