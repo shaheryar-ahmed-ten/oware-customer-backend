3d23cb214a3024808dbb2e0c0fe424d0ed802175
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-07-29 18:03:33 +0500

    seeders bug fix

--- seeders/20210526104945-create-role-permissions.js
+++ seeders/20210526104945-create-role-permissions.js
@@ -1,40 +1,44 @@
-'use strict';
+"use strict";
 const { Op } = require("sequelize");
-const { Role, PermissionAccess, Permission } = require('../models')
-const permissionEnums = require('../enums/permissions');
+const { Role, PermissionAccess, Permission } = require("../models");
+const permissionEnums = require("../enums/permissions");
 
 module.exports = {
   up: async () => {
-    let roles = await Role.bulkCreate([{
-      name: 'Customer Super Admin',
-      type: 'CUSTOMER_SUPER_ADMIN'
-    }, {
-      name: 'Customer Admin',
-      type: 'CUSTOMER_ADMIN'
-    }]);
-    const superAdminRole = roles.find(role => role.type == 'CUSTOMER_SUPER_ADMIN');
+    let roles = await Role.bulkCreate([
+      {
+        name: "Customer Super Admin",
+        type: "CUSTOMER_SUPER_ADMIN"
+      },
+      {
+        name: "Customer Admin",
+        type: "CUSTOMER_ADMIN"
+      }
+    ]);
+    const superAdminRole = roles.find(role => role.type == "CUSTOMER_SUPER_ADMIN");
     const superAdminPermissions = await Permission.findAll({
       where: {
         type: {
-          [Op.in]: Object.keys(permissionEnums).filter(type => type.indexOf('_FULL') > -1)
+          [Op.in]: Object.keys(permissionEnums).filter(type => type.indexOf("_FULL") > -1)
         }
       }
     });
     let permissionAccesses = await PermissionAccess.bulkCreate(
       superAdminPermissions.map(permission => ({
         roleId: superAdminRole.id,
-        permissionId: permission.id,
-      })));
+        permissionId: permission.id
+      }))
+    );
     return [roles, permissionAccesses];
   },
 
   down: async () => {
-    const superAdminRole = await Role.findOne({ where: { type: 'CUSTOMER_SUPER_ADMIN' } });
-    const adminRole = await Role.findOne({ where: { type: 'CUSTOMER_SUPER_ADMIN' } });
+    const superAdminRole = await Role.findOne({ where: { type: "CUSTOMER_SUPER_ADMIN" } });
+    const adminRole = await Role.findOne({ where: { type: "CUSTOMER_SUPER_ADMIN" } });
     const permissions = await Permission.findAll({
       where: {
         type: {
-          [Op.in]: Object.keys(permissionEnums).filter(type => type.indexOf('_FULL') > -1)
+          [Op.in]: Object.keys(permissionEnums).filter(type => type.indexOf("_FULL") > -1)
         }
       }
     });
