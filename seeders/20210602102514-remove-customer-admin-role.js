7b1f55a17ee5f97fdfa1668924b8922e8afc7a8f
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-07-29 16:55:50 +0500

    bug fixes

--- seeders/20210602102514-remove-customer-admin-role.js
+++ seeders/20210602102514-remove-customer-admin-role.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 const { Op } = require("sequelize");
-const { Role, PermissionAccess, Permission } = require('../models')
-const permissionEnums = require('../enums/permissions');
+const { Role, PermissionAccess, Permission } = require("../models");
+const permissionEnums = require("../enums/permissions");
 
 module.exports = {
   up: async () => {
-    const adminRole = await Role.findOne({ where: { type: 'CUSTOMER_ADMIN' } });
+    const adminRole = await Role.findOne({ where: { type: "CUSTOMER_ADMIN" } });
     await PermissionAccess.destroy({
       where: {
         roleId: adminRole.id
@@ -20,8 +20,9 @@ module.exports = {
 
   down: async () => {
     let adminRole = await Role.create({
-      name: 'Customer Admin',
-      type: 'CUSTOMER_ADMIN'
+      name: "Customer Admin",
+      type: "CUSTOMER_ADMIN",
+      allowedApps: "CUSTOMER"
     });
     return [adminRole];
   }
