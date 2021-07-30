2dd9605c540f588ef42ac4175098734a9e6a1624
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-07-30 16:05:23 +0500

    ride permission change to dashboard

--- api/index.js
+++ api/index.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userRouter = require('./user');
-const dashboardRouter = require('./dashboard');
-const inwardRouter = require('./inward')
-const orderRouter = require('./order')
-const productRouter = require('./product')
-const rideRouter = require('./ride')
+const userRouter = require("./user");
+const dashboardRouter = require("./dashboard");
+const inwardRouter = require("./inward");
+const orderRouter = require("./order");
+const productRouter = require("./product");
+const rideRouter = require("./ride");
 
-
-const { isLoggedIn, checkPermission } = require('../services/auth.service');
-const { PERMISSIONS } = require('../enums');
+const { isLoggedIn, checkPermission } = require("../services/auth.service");
+const { PERMISSIONS } = require("../enums");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.json({
     success: true,
-    message: 'Welcome!'
+    message: "Welcome!"
   });
 });
 
-router.use('/user', userRouter);
-router.use('/dashboard', isLoggedIn, checkPermission(PERMISSIONS.CP_DASHBOARD_FULL), dashboardRouter);
-router.use('/inward', isLoggedIn, checkPermission(PERMISSIONS.CP_INWARD_FULL), inwardRouter);
-router.use('/order', isLoggedIn, checkPermission(PERMISSIONS.CP_ORDER_FULL), orderRouter);
-router.use('/product', isLoggedIn, checkPermission(PERMISSIONS.CP_PRODUCT_FULL), productRouter);
-router.use("/ride", isLoggedIn, checkPermission(PERMISSIONS.CP_RIDE_FULL),rideRouter)
+router.use("/user", userRouter);
+router.use("/dashboard", isLoggedIn, checkPermission(PERMISSIONS.CP_DASHBOARD_FULL), dashboardRouter);
+router.use("/inward", isLoggedIn, checkPermission(PERMISSIONS.CP_INWARD_FULL), inwardRouter);
+router.use("/order", isLoggedIn, checkPermission(PERMISSIONS.CP_ORDER_FULL), orderRouter);
+router.use("/product", isLoggedIn, checkPermission(PERMISSIONS.CP_PRODUCT_FULL), productRouter);
+router.use("/ride", isLoggedIn, checkPermission(PERMISSIONS.CP_DASHBOARD_FULL), rideRouter);
 
 module.exports = router;
