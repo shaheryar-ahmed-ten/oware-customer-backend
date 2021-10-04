c0daaf0f356054bcfeb56e80069a58331cf6cbd7
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-10-04 11:58:40 +0500

    user inactive error meesage

--- api/user.js
+++ api/user.js
@@ -14,7 +14,7 @@ async function updateUser(req, res, next) {
   if (!user)
     return res.status(400).json({
       success: false,
-      message: "No user found!"
+      message: "No user found!",
     });
   user.firstName = req.body.firstName;
   user.lastName = req.body.lastName;
@@ -28,12 +28,12 @@ async function updateUser(req, res, next) {
     return res.json({
       success: true,
       message: "Your profile data updated successfully",
-      data: response
+      data: response,
     });
   } catch (err) {
     return res.json({
       success: false,
-      message: err.errors.pop().message
+      message: err.errors.pop().message,
     });
   }
 }
@@ -43,36 +43,41 @@ router.post("/auth/login", async (req, res, next) => {
   let loginKey = req.body.username.indexOf("@") > -1 ? "email" : "username";
   const user = await User.findOne({
     where: { [loginKey]: req.body.username },
-    include: [{ model: Company, as: "Company" }, Role]
+    include: [{ model: Company, as: "Company" }, Role],
   });
   if (!user)
     return res.status(401).json({
       success: false,
-      message: "User doesn't exist with this email!"
+      message: "User doesn't exist with this email!",
+    });
+  if (!user.isActive)
+    return res.status(400).json({
+      success: false,
+      message: "User is inactive",
     });
   let isPasswordValid = user.comparePassword(req.body.password);
   if (!isPasswordValid)
     return res.status(401).json({
       success: false,
-      message: "Invalid password!"
+      message: "Invalid password!",
     });
   if (user.Role.allowedApps.split(",").indexOf(APPS.CUSTOMER) < 0)
     return res.status(401).json({
       status: false,
-      message: "Not allowed to enter customer portal"
+      message: "Not allowed to enter customer portal",
     });
   if (!user.companyId)
     return res.status(401).json({
       status: false,
-      message: "User is not assigned to any company!"
+      message: "User is not assigned to any company!",
     });
   var token = jwt.sign({ id: user.id }, config.JWT_SECRET, {
-    expiresIn: 86400 // expires in 24 hours
+    expiresIn: 86400, // expires in 24 hours
   });
   res.json({
     success: isPasswordValid,
     message: "Login successful",
-    token
+    token,
   });
 });
 
@@ -80,7 +85,7 @@ router.post("/auth/login", async (req, res, next) => {
 router.get("/me", authService.isLoggedIn, async (req, res, next) => {
   return res.json({
     success: true,
-    data: req.user
+    data: req.user,
   });
 });
 
@@ -100,13 +105,14 @@ router.patch("/me/password", authService.isLoggedIn, async (req, res, next) => {
   if (!user)
     return res.json({
       success: false,
-      message: "No user found!"
+      message: "No user found!",
     });
+
   let isPasswordValid = user.comparePassword(req.body.oldPassword);
   if (!isPasswordValid)
     return res.json({
       success: false,
-      message: "Invalid Old Password!"
+      message: "Invalid Old Password!",
     });
   user.password = req.body.password;
   try {
@@ -115,12 +121,12 @@ router.patch("/me/password", authService.isLoggedIn, async (req, res, next) => {
     return res.json({
       success: true,
       message: "Your profile data updated successfully",
-      data: response
+      data: response,
     });
   } catch (err) {
     return res.json({
       success: false,
-      message: err.message
+      message: err.message,
     });
   }
 });
@@ -131,30 +137,30 @@ router.post("/auth/forgot-password", async (req, res, next) => {
   if (!user)
     return res.status(400).json({
       success: false,
-      message: "User doesn't exist with this email"
+      message: "User doesn't exist with this email",
     });
   if (!user.isActive)
     return res.status(400).json({
       success: false,
-      message: "User is inactive"
+      message: "User is inactive",
     });
   const otp = "12345";
   await VerificationCode.create({
     userId: user.id,
     code: otp,
     identity: user.id,
-    expiryDate: moment().add(1, "hour").toDate()
+    expiryDate: moment().add(1, "hour").toDate(),
   });
 
   sendForgotPasswordOTPEmail({
     email: req.body.email,
     otp,
     name: `${user.firstName} ${user.lastName}`,
-    link: `${req.get("referrer")}forgot-password/change-password/${user.id}/${otp}`
+    link: `${req.get("referrer")}forgot-password/change-password/${user.id}/${otp}`,
   });
   return res.json({
     success: true,
-    message: "Forgot password email sent!"
+    message: "Forgot password email sent!",
   });
 });
 
@@ -164,39 +170,38 @@ router.post("/auth/change-password/:id/:otp", async (req, res, next) => {
   if (!user)
     return res.status(400).json({
       success: false,
-      message: "User doesn't exist with this email"
+      message: "User doesn't exist with this email",
     });
   if (!user.isActive)
     return res.status(400).json({
       success: false,
-      message: "User is inactive"
+      message: "User is inactive",
     });
   let code = await VerificationCode.findOne({ where: { code: req.params.otp, identity: req.params.id } });
   if (!code)
     return res.status(401).json({
       success: false,
-      message: "Invalid otp"
+      message: "Invalid otp",
     });
   user.password = req.body.password;
   user.save();
   await VerificationCode.destroy({ where: { identity: req.params.id } });
   return res.json({
     success: true,
-    message: "Your profile data updated successfully"
+    message: "Your profile data updated successfully",
   });
 });
 
-
 // GET Company Data
-router.get('/company', authService.isLoggedIn, async (req, res, next) => {
+router.get("/company", authService.isLoggedIn, async (req, res, next) => {
   // const limit = req.query.rowsPerPage || config.rowsPerPage;
   // const offset = (req.query.page - 1 || 0) * limit;
   // let where = {
-      // id: req.companyId,
+  // id: req.companyId,
   //     // productId: req.params.id
   // };
-  const  companyId  = await Company.findOne({ where: { id: req.companyId } });
-  const fileid = await File.findOne({where: {id: companyId.logoId}});
+  const companyId = await Company.findOne({ where: { id: req.companyId } });
+  const fileid = await File.findOne({ where: { id: companyId.logoId } });
   // const response = await Company.findAndCountAll({
   //     attributes: [
   //         'logoId'
@@ -217,12 +222,12 @@ router.get('/company', authService.isLoggedIn, async (req, res, next) => {
   // });
   // const response= 'Checked';
   return res.json({
-        success: true,
-        message: 'respond with a resource',
-        data: companyId,
-        file: fileid
-        // pages: Math.ceil(response.count / limit)
-    });
+    success: true,
+    message: "respond with a resource",
+    data: companyId,
+    file: fileid,
+    // pages: Math.ceil(response.count / limit)
+  });
   // return res.json({
   //   success: true,
   //   data: req.user
