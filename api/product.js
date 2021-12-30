0144d57ccb4bac6ab849e9173e64ce29e57eeb80
shaheryar0129 <81627809+shaheryar0129@users.noreply.github.com>
2021-12-30 16:44:11 +0500

    product invenory batches (#88)

--- api/product.js
+++ api/product.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { Warehouse, Product, UOM, Inventory, Category, Brand, InboundStat, sequelize } = require("../models");
+const {
+  Warehouse,
+  Product,
+  UOM,
+  Inventory,
+  Category,
+  Brand,
+  InboundStat,
+  sequelize,
+  InventoryDetail,
+} = require("../models");
 const config = require("../config");
 const { Op, Sequelize } = require("sequelize");
 const moment = require("moment-timezone");
 const ExcelJS = require("exceljs");
+const httpStatus = require("http-status");
 
 /* GET orders listing. */
 router.get("/", async (req, res, next) => {
   const limit = req.query.rowsPerPage || config.rowsPerPage;
   const offset = (req.query.page - 1 || 0) * limit;
   let where = {
-    customerId: req.companyId
+    customerId: req.companyId,
   };
   // where.push({ customerId: req.companyId });
   // if (req.query.search)
@@ -21,15 +32,15 @@ router.get("/", async (req, res, next) => {
   //     }))
   //   );
   if (req.query.search)
-  where[Op.or] = ["$Warehouse.name$", "$Product.name$", "$Product.Category.name$"].map((key) => ({
-    [key]: { [Op.like]: "%" + req.query.search + "%" },
-  }));
+    where[Op.or] = ["$Warehouse.name$", "$Product.name$", "$Product.Category.name$"].map((key) => ({
+      [key]: { [Op.like]: "%" + req.query.search + "%" },
+    }));
 
   if (req.query.days) {
     const currentDate = moment();
     const previousDate = moment().subtract(req.query.days, "days");
     where["createdAt"] = { [Op.between]: [previousDate, currentDate] };
-  } else if (req.query.start&& req.query.end) {
+  } else if (req.query.start && req.query.end) {
     const startDate = moment(req.query.start);
     const endDate = moment(req.query.end).set({
       hour: 23,
@@ -40,40 +51,86 @@ router.get("/", async (req, res, next) => {
     where["createdAt"] = { [Op.between]: [startDate, endDate] };
   }
 
-    const response = await Inventory.findAll({
-        include: [{ model: Product, attributes: ['name'], include: [{ model: Category, attributes: ['name'] }, { model: Brand, attributes: ['name'] }, { model: UOM, attributes: ['name'] }],required:true },{model: Warehouse, attributes: ['name'],required:true}],
-        attributes: [
-            ['productId', 'id'],
-            [Sequelize.fn('sum', Sequelize.col('committedQuantity')), 'committedQuantity'],
-            [Sequelize.fn('sum', Sequelize.col('availableQuantity')), 'availableQuantity'],
-            [Sequelize.fn('sum', Sequelize.col('dispatchedQuantity')), 'dispatchedQuantity']
+  const response = await Inventory.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["name"],
+        include: [
+          { model: Category, attributes: ["name"] },
+          { model: Brand, attributes: ["name"] },
+          { model: UOM, attributes: ["name"] },
         ],
-        where, 
-        offset, limit,
-        group: ['productId','warehouseId']
-    })
-    const count = await Inventory.count({
-        // distinct: true,
-        include: [{ model: Product, attributes: ['name'], include: [{ model: Category, attributes: ['name'] }, { model: Brand, attributes: ['name'] }, { model: UOM, attributes: ['name'] }],required:true },{model: Warehouse, attributes: ['name'],required:true}],
-        col: 'productId',
-        where
+        required: true,
+      },
+      { model: Warehouse, attributes: ["name"], required: true },
+      { model: InventoryDetail, as: "InventoryDetail" },
+    ],
+    attributes: [
+      ["productId", "id"],
+      [Sequelize.fn("sum", Sequelize.col("committedQuantity")), "committedQuantity"],
+      [Sequelize.fn("sum", Sequelize.col("availableQuantity")), "availableQuantity"],
+      [Sequelize.fn("sum", Sequelize.col("dispatchedQuantity")), "dispatchedQuantity"],
+    ],
+    where,
+    offset,
+    limit,
+    group: ["productId", "warehouseId"],
+  });
+  const count = await Inventory.count({
+    // distinct: true,
+    include: [
+      {
+        model: Product,
+        attributes: ["name"],
+        include: [
+          { model: Category, attributes: ["name"] },
+          { model: Brand, attributes: ["name"] },
+          { model: UOM, attributes: ["name"] },
+        ],
+        required: true,
+      },
+      { model: Warehouse, attributes: ["name"], required: true },
+    ],
+    col: "productId",
+    where,
+  });
+
+  res.json({
+    success: true,
+    message: "respond with a resource",
+    data: response,
+    count,
+    pages: Math.ceil(count / limit),
+  });
+});
+
+router.get("/batches/:inventoryId", async (req, res, next) => {
+  try {
+    const batches = await InventoryDetail.findAll({
+      where: { inventoryId: req.params.inventoryId },
+      logging: console.log,
     });
-    
     res.json({
-        success: true,
-        message: 'respond with a resource',
-        data: response,
-        count,
-        pages: Math.ceil(count / limit)
+      success: true,
+      message: "respond with a resource",
+      batches,
     });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err.message,
+      data: null,
+    });
+  }
 });
 
 router.get("/relations", async (req, res, next) => {
   const whereClauseWithoutDate = {
     customerId: req.companyId,
     availableQuantity: {
-      [Op.ne]: 0
-    }
+      [Op.ne]: 0,
+    },
   };
   const relations = {
     products: await sequelize.query(
@@ -81,13 +138,13 @@ router.get("/relations", async (req, res, next) => {
         from Inventories join Products as product on product.id = Inventories.productId 
         where customerId = ${req.companyId} and availableQuantity != 0;`,
       { type: Sequelize.QueryTypes.SELECT }
-    )
+    ),
   };
 
   res.json({
     success: true,
     message: "respond with a resource",
-    relations
+    relations,
   });
 });
 
@@ -113,16 +170,16 @@ router.get("/export", async (req, res, next) => {
     "QUANTITY COMMITTED",
     "QUANTITY DISPATCHED",
   ]);
-  
+
   if (req.query.search)
-  where[Op.or] = ["$Warehouse.name$", "$Product.name$", "$Product.Category.name$"].map((key) => ({
-    [key]: { [Op.like]: "%" + req.query.search + "%" },
-  }));
+    where[Op.or] = ["$Warehouse.name$", "$Product.name$", "$Product.Category.name$"].map((key) => ({
+      [key]: { [Op.like]: "%" + req.query.search + "%" },
+    }));
   if (req.query.days) {
     const currentDate = moment();
     const previousDate = moment().subtract(req.query.days, "days");
     where["createdAt"] = { [Op.between]: [previousDate, currentDate] };
-  } else if (req.query.start&& req.query.end) {
+  } else if (req.query.start && req.query.end) {
     const startDate = moment(req.query.start);
     const endDate = moment(req.query.end).set({
       hour: 23,
@@ -133,62 +190,73 @@ router.get("/export", async (req, res, next) => {
     where["createdAt"] = { [Op.between]: [startDate, endDate] };
   }
   const response = await Inventory.findAll({
-    include: [{ model: Product, attributes: ['name'], include: [{ model: Category, attributes: ['name'],required:true }, { model: Brand, attributes: ['name'] }, { model: UOM, attributes: ['name'] }],required:true },{model: Warehouse, attributes: ['name'],required:true}],
-    where, 
+    include: [
+      {
+        model: Product,
+        attributes: ["name"],
+        include: [
+          { model: Category, attributes: ["name"], required: true },
+          { model: Brand, attributes: ["name"] },
+          { model: UOM, attributes: ["name"] },
+        ],
+        required: true,
+      },
+      { model: Warehouse, attributes: ["name"], required: true },
+    ],
+    where,
     // offset, limit,
     // group: ['productId','warehouseId']
-})
+  });
 
-worksheet.addRows(
-  response.map((row) => [
-    row.Product.name,
-    row.Product.Category.name,
-    row.Warehouse.name,
-    row.Product.Brand.name,
-    row.Product.UOM.name,
-    row.availableQuantity,
-    row.committedQuantity,
-    row.dispatchedQuantity,
-  ])
-);
+  worksheet.addRows(
+    response.map((row) => [
+      row.Product.name,
+      row.Product.Category.name,
+      row.Warehouse.name,
+      row.Product.Brand.name,
+      row.Product.UOM.name,
+      row.availableQuantity,
+      row.committedQuantity,
+      row.dispatchedQuantity,
+    ])
+  );
 
-res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-res.setHeader("Content-Disposition", "attachment; filename=" + "Inventory.xlsx");
+  res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+  res.setHeader("Content-Disposition", "attachment; filename=" + "Inventory.xlsx");
 
-await workbook.xlsx.write(res).then(() => res.end());
-
+  await workbook.xlsx.write(res).then(() => res.end());
 });
 
-router.get('/:id', async (req, res, next) => {
-    const limit = req.query.rowsPerPage || config.rowsPerPage;
-    const offset = (req.query.page - 1 || 0) * limit;
-    let where = {
-        customerId: req.companyId,
-        productId: req.params.id
-    };
-    const response = await Inventory.findAndCountAll({
-        attributes: [
-            'availableQuantity', 'committedQuantity','dispatchedQuantity'
-        ],
-        include: [{
-            model: Product, attributes: []
-        }, {
-            model: Warehouse, attributes: [
-                'name'
-            ]
-        }],
-        order: [['createdAt', 'DESC']],
-        where, limit, offset,
-    });
-    res.json({
-        success: true,
-        message: 'respond with a resource',
-        data: response.rows,
-        pages: Math.ceil(response.count / limit)
-    });
+router.get("/:id", async (req, res, next) => {
+  const limit = req.query.rowsPerPage || config.rowsPerPage;
+  const offset = (req.query.page - 1 || 0) * limit;
+  let where = {
+    customerId: req.companyId,
+    productId: req.params.id,
+  };
+  const response = await Inventory.findAndCountAll({
+    attributes: ["availableQuantity", "committedQuantity", "dispatchedQuantity"],
+    include: [
+      {
+        model: Product,
+        attributes: [],
+      },
+      {
+        model: Warehouse,
+        attributes: ["name"],
+      },
+    ],
+    order: [["createdAt", "DESC"]],
+    where,
+    limit,
+    offset,
+  });
+  res.json({
+    success: true,
+    message: "respond with a resource",
+    data: response.rows,
+    pages: Math.ceil(response.count / limit),
+  });
 });
-
-
-
 
 module.exports = router;
